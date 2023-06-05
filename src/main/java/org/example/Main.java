f461a8e290cfc56c9a61ff36d81797e40402e4e7
eldar <eldar.zaripoff@gmail.com>
2023-06-05 23:28:10 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("10101", "11110"));
+    }
+}
